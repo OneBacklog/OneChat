fb6b03a76fff9de274401f069364921ad5c01ac2
Irfan Fadilah <hello@irfanfadilah.com>
2025-01-30 23:03:34 +0700

    Update List of Models

--- main.js
+++ main.js
@@ -13,10 +13,11 @@ createApp({
     loading: false,
     input: '',
     models: [
-      { value: 'gpt-4o-mini', text: 'GPT-4o Mini' },
       { value: 'gpt-4o', text: 'GPT-4o' },
+      { value: 'gpt-4o-mini', text: 'GPT-4o Mini' },
+      { value: 'o1', text: 'o1' },
       { value: 'o1-mini', text: 'o1 Mini' },
-      { value: 'o1-preview', text: 'o1 Preview' },
+      { value: 'DeepSeek-R1', text: 'DeepSeek R1' },
       { value: 'Llama-3.3-70B-Instruct', text: 'Llama 3.3' }
     ]
   }),
@@ -25,7 +26,7 @@ createApp({
       return 'Chat with ' + this.models.find(model => model.value === this.settings.model).text
     },
     is_o1() {
-      return ['o1-mini', 'o1-preview'].includes(this.settings.model)
+      return ['o1', 'o1-mini'].includes(this.settings.model)
     }
   },
   mounted() {
@@ -73,12 +74,12 @@ createApp({
           messages: messages
         }
       } else {
-        if (messages.find(message => message.role == 'system') == undefined) {
+        if (this.settings.model != 'DeepSeek-R1' && messages.find(message => message.role == 'system') == undefined) {
           messages.unshift({ role: 'system', content: this.settings.system })
         }
 
         let max_tokens = 16384
-        if (this.settings.model == 'Llama-3.3-70B-Instruct') { max_tokens = 4096 }
+        if (['Llama-3.3-70B-Instruct', 'DeepSeek-R1'].includes(this.settings.model)) { max_tokens = 4096 }
 
         return {
           model: this.settings.model,
@@ -105,7 +106,7 @@ createApp({
           this.messages.push({ role: 'assistant', content: `Error: ${data.error.message}` })
           if (data.error.code == 'unavailable_model') {
             const model = this.models.find(model => model.value === this.settings.model)
-            this.messages.push({ role: 'assistant', content: `You may need a GitHub Copilot subscription to use ${model}.` })
+            this.messages.push({ role: 'assistant', content: `You may need a GitHub Copilot subscription to use ${model.text}.` })
           }
         }
         this.scroll()
