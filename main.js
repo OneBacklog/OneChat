0d4876d85f380aa193f90e464eb2042a4027956b
Irfan Fadilah <hello@irfanfadilah.com>
2025-02-01 05:29:46 +0700

    Refactoring

--- main.js
+++ main.js
@@ -2,33 +2,32 @@ const { createApp } = Vue
 
 createApp({
   data: () => ({
-    open: false,
     settings: {
       token: localStorage.getItem('token') || '',
       system: localStorage.getItem('system') || '',
-      model: localStorage.getItem('model') || 'gpt-4o-mini'
+      model: localStorage.getItem('model') || 'gpt-4o-mini',
+      open: false
     },
     messages: JSON.parse(localStorage.getItem('messages')) || [],
     total_tokens: localStorage.getItem('total_tokens') || 0,
     loading: false,
     input: '',
     models: [
-      { value: 'o1', text: 'o1' },
-      { value: 'o1-mini', text: 'o1 Mini' },
-      { value: 'o3', text: 'o3' },
-      { value: 'o3-mini', text: 'o3 Mini' },
-      { value: 'DeepSeek-R1', text: 'DeepSeek R1' },
-      { value: 'gpt-4o', text: 'GPT-4o' },
-      { value: 'gpt-4o-mini', text: 'GPT-4o Mini' },
-      { value: 'Llama-3.3-70B-Instruct', text: 'Llama 3.3' }
+      { name: 'o1', value: 'o1' },
+      { name: 'o1 Mini', value: 'o1-mini' },
+      { name: 'o3 Mini', value: 'o3-mini' },
+      { name: 'DeepSeek R1', value: 'DeepSeek-R1' },
+      { name: 'GPT-4o', value: 'gpt-4o' },
+      { name: 'GPT-4o Mini', value: 'gpt-4o-mini' },
+      { name: 'Llama 3.3', value: 'Llama-3.3-70B-Instruct' }
     ]
   }),
   computed: {
     placeholder() {
-      return 'Chat with ' + this.models.find(model => model.value === this.settings.model).text
+      return 'Chat with ' + this.models.find(model => model.value === this.settings.model).name
     },
     is_oX() {
-      return ['o1', 'o1-mini', 'o3', 'o3-mini'].includes(this.settings.model)
+      return ['o1', 'o1-mini', 'o3-mini'].includes(this.settings.model)
     },
     is_reasoning() {
       return this.settings.model == 'DeepSeek-R1' || this.is_oX
@@ -45,14 +44,14 @@ createApp({
   },
   methods: {
     toggle() {
-      this.open = !this.open
+      this.settings.open = !this.settings.open
     },
     save() {
       if (localStorage.getItem('model') != this.settings.model) this.clear(true)
       localStorage.setItem('system', this.settings.system)
       localStorage.setItem('token', this.settings.token)
       localStorage.setItem('model', this.settings.model)
-      this.open = false
+      this.settings.open = false
     },
     scroll(block='end') {
       setTimeout(() => document.querySelector("ul#chat > li:last-child")?.scrollIntoView({ behavior: 'smooth', block: block }))
@@ -166,7 +165,7 @@ createApp({
 
       if (data.error.code == 'unavailable_model') {
         const model = this.models.find(model => model.value === this.settings.model)
-        content += `<br>You may need a GitHub Copilot Pro to use ${model.text}.`
+        content += `<br>You may need GitHub Copilot Pro subscription to use ${model.name}.`
       }
 
       this.messages[this.messages.length - 1].content = content
